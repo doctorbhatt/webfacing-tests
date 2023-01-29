ca7ce88b8e6cff1dc11ceb81c206a7da4d3d2e78
Doctorbhatt <doctorbhatt@gmail.com>
2023-01-28 23:02:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print "Hello there"
+print "If you can read this, you have successfully executed a Python test"
+
+print "For loop test"
+
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+else:
+    print("No items left.")
